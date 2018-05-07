c9b991226669bf38e39e54ecacf9d87f5879d8ba
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-05-06 23:01:57 -0400

    fix some bugs and added a category section when listing videos

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,11 +6,12 @@ defmodule Doggygram.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+    has_many :videos, Doggygram.Video
 
-    timestamps
+    timestamps()
   end
 
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,7 @@ defmodule Doggygram.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Doggygram.User
+    belongs_to :category, Doggygram.Category
 
     timestamps()
   end
@@ -13,9 +14,10 @@ defmodule Doggygram.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:url, :title, :description, :category_id])
+    |> assoc_constraint(:category)
   end
 end
